9e3e146aae417efb0e035f9acb3425b1b83a2a4e
Up <upcraftedlp@gmail.com>
2023-06-26 21:22:12 +0200

    fix config screens

--- common/src/main/java/mod/crend/autohud/screen/ConfigScreenFactory.java
+++ common/src/main/java/mod/crend/autohud/screen/ConfigScreenFactory.java
@@ -1,16 +1,12 @@
 package mod.crend.autohud.screen;
 
-import dev.isxander.yacl.api.YetAnotherConfigLib;
-import mod.crend.autohud.config.Config;
-import mod.crend.autohud.config.ConfigHandler;
-import mod.crend.autoyacl.AutoYacl;
+import eu.midnightdust.lib.config.MidnightConfig;
+import mod.crend.autohud.AutoHud;
 import net.minecraft.client.gui.screen.Screen;
 
 public class ConfigScreenFactory {
 	public static Screen makeScreen(Screen parent) {
-		return YetAnotherConfigLib.create(ConfigHandler.CONFIG_STORE.withYacl().instance,
-				(defaults, config, builder) -> AutoYacl.parse(Config.class, defaults, config, builder)
-		).generateScreen(parent);
+		return MidnightConfig.getScreen(parent, AutoHud.MOD_ID);
 	}
 
 }
--- forge/src/main/java/mod/crend/autohud/forge/AutoHudModEvents.java
+++ forge/src/main/java/mod/crend/autohud/forge/AutoHudModEvents.java
@@ -4,8 +4,7 @@ import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import mod.crend.autohud.AutoHud;
 import mod.crend.autohud.ModKeyBindings;
 import mod.crend.autohud.api.AutoHudApi;
-import mod.crend.autohud.screen.ConfigScreenWrapper;
-import mod.crend.autoyacl.YaclHelper;
+import mod.crend.autohud.screen.ConfigScreenFactory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
@@ -18,31 +17,29 @@ import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 
 @Mod.EventBusSubscriber(modid = AutoHud.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class AutoHudModEvents {
-	public static final String REGISTER_API = "register_api";
+    public static final String REGISTER_API = "register_api";
 
-	@SubscribeEvent
-	static void onClientSetup(FMLClientSetupEvent event) {
-		MixinExtrasBootstrap.init();
-		AutoHud.init();
-		if (YaclHelper.HAS_YACL) {
-			ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
-					() -> new ConfigScreenHandler.ConfigScreenFactory(
-							(minecraft, screen) -> ConfigScreenWrapper.getScreen(screen)
-					)
-			);
-		}
-	}
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        MixinExtrasBootstrap.init();
+        AutoHud.init();
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory(
+                        (minecraft, screen) -> ConfigScreenFactory.makeScreen(screen)
+                )
+        );
+    }
 
-	@SubscribeEvent
-	static void onInterModProcess(InterModProcessEvent event) {
-		InterModComms.getMessages(AutoHud.MOD_ID, REGISTER_API::equals)
-				.map(msg -> (AutoHudApi) msg.messageSupplier().get())
-				.forEach(AutoHud::addApi);
-	}
+    @SubscribeEvent
+    static void onInterModProcess(InterModProcessEvent event) {
+        InterModComms.getMessages(AutoHud.MOD_ID, REGISTER_API::equals)
+                .map(msg -> (AutoHudApi) msg.messageSupplier().get())
+                .forEach(AutoHud::addApi);
+    }
 
-	@SubscribeEvent
-	static void onKeyMappingsRegister(RegisterKeyMappingsEvent event) {
-		ModKeyBindings.ALL.forEach(event::register);
-	}
+    @SubscribeEvent
+    static void onKeyMappingsRegister(RegisterKeyMappingsEvent event) {
+        ModKeyBindings.ALL.forEach(event::register);
+    }
 
 }
